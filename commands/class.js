9a4383cfdeae55416100bc2c858944d765aa70f7
LavaTheif <charlie@lavatheif.com>
2021-02-12 14:41:52 +0000

    added !class [group number] command
    Gets a users class links
    
        new file:   commands/class.js

---
+++ commands/class.js
@@ -0,0 +1,91 @@
+const linkData = [
+    "__Group 1 -- Thursday__\n\n" +
+    "10am: https://herts-ac-uk.zoom.us/j/97724027308?pwd=YXZiVkNLVTNTVWpGU20wQlg1NXBnUT09\n" +
+    "1pm: https://herts.instructure.com/courses/81524/modules/items/1414494\n" +
+    "3pm: https://herts.instructure.com/courses/81524/modules/items/1414492\n",
+
+    "__Group 2 -- Thursday__\n\n" +
+    "10am: https://herts.instructure.com/courses/81524/modules/items/1414496\n" +
+    "11am: https://herts.instructure.com/courses/81524/modules/items/1414495\n" +
+    "2pm: https://teams.microsoft.com/l/meetup-join/19%3ameeting_YjQyNjVkMDItMTY1OC00ZWEwLTk5MWQtOGRhN2M2ZTJkMGY1%40thread.v2/0?context=%7b%22Tid%22%3a%2293e6beba-c4aa-4731-af5d-d735b097eadb%22%2c%22Oid%22%3a%220e65d301-8d52-44d5-b211-9d62396d8265%22%7d\n",
+
+    "__Group 3 -- Friday__\n\n" +
+    "10am: https://herts-ac-uk.zoom.us/j/97724027308?pwd=YXZiVkNLVTNTVWpGU20wQlg1NXBnUT09\n" +
+    "1pm: https://herts.instructure.com/courses/81524/modules/items/1414488\n" +
+    "2pm: https://herts.instructure.com/courses/81524/modules/items/1403527\n",
+
+    "__Group 4 -- Thursday__\n\n" +
+    "10am: https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZDViMzE1YjAtODJjNy00YzE2LThlNmQtN2VlZDhjYjc0YjQ4%40thread.v2/0?context=%7b%22Tid%22%3a%2293e6beba-c4aa-4731-af5d-d735b097eadb%22%2c%22Oid%22%3a%220e65d301-8d52-44d5-b211-9d62396d8265%22%7d\n" +
+    "2pm: https://herts.instructure.com/courses/81524/modules/items/1414493\n" +
+    "4pm: https://herts.instructure.com/courses/81524/modules/items/1414490\n",
+
+    "__Group 5 -- Friday__\n\n" +
+    "11am: https://herts.instructure.com/courses/81524/modules/items/1416886\n" +
+    "12pm: https://herts.instructure.com/courses/81524/modules/items/1416887\n" +
+    "2pm: https://herts-ac-uk.zoom.us/j/93277749199?pwd=UU9RdTd2N0UraElxell6Uk1jMUNVdz09\n",
+
+    "__Group 6 -- Thursday__\n\n" +
+    "10am: https://herts.instructure.com/courses/81524/modules/items/1416883\n" +
+    "11am: https://herts.instructure.com/courses/81524/modules/items/1416885\n" +
+    "1pm: https://herts-ac-uk.zoom.us/j/93828672926?pwd=Q1JjZ0ZkL25xeXVOcmk1aXFNRmp0QT09\n",
+
+    "__Group 7 -- Friday__\n\n" +
+    "10am: https://herts-ac-uk.zoom.us/j/93277749199?pwd=UU9RdTd2N0UraElxell6Uk1jMUNVdz09\n" +
+    "1pm: https://herts.instructure.com/courses/81524/modules/items/1416895\n" +
+    "2pm: https://herts.instructure.com/courses/81524/modules/items/1416896\n",
+
+    "__Group 8 -- Thursday__\n\n" +
+    "12pm: https://herts.instructure.com/courses/81524/modules/items/1416890\n" +
+    "2pm: https://herts.instructure.com/courses/81524/modules/items/1416891\n" +
+    "3pm: https://herts-ac-uk.zoom.us/j/97724027308?pwd=YXZiVkNLVTNTVWpGU20wQlg1NXBnUT09\n",
+
+    "__Group 9 -- Thursday__\n\n" +
+    "10am: https://herts.instructure.com/courses/81524/modules/items/1416757\n" +
+    "11am: https://herts.instructure.com/courses/81524/modules/items/1416858\n" +
+    "1pm: https://herts-ac-uk.zoom.us/j/94076004164?pwd=Yk9CWVM1b0tXMUZwWlZjQmlWazdkZz09\n"+
+    "\n__Friday__\n\n1pm: https://herts-ac-uk.zoom.us/j/94076004164?pwd=Yk9CWVM1b0tXMUZwWlZjQmlWazdkZz09\n",
+
+    "__Group 10 -- Friday__\n\n" +
+    "10am: https://herts-ac-uk.zoom.us/j/92121551146\n" +
+    "1pm: https://herts.instructure.com/courses/81524/modules/items/1416901\n" +
+    "3pm: https://herts.instructure.com/courses/81524/modules/items/1416902\n",
+
+    "__Group 11 -- Friday__\n\n" +
+    "10am: https://herts.instructure.com/courses/81524/modules/items/1416860\n" +
+    "12pm: https://herts.instructure.com/courses/81524/modules/items/1416863\n" +
+    "2pm: https://herts-ac-uk.zoom.us/j/92121551146\n",
+
+    "__Group 12 -- Friday__\n\n" +
+    "11am: https://herts.instructure.com/courses/81524/modules/items/1416862\n" +
+    "1pm: https://herts-ac-uk.zoom.us/j/94076004164?pwd=Yk9CWVM1b0tXMUZwWlZjQmlWazdkZz09\n" +
+    "3pm: https://herts.instructure.com/courses/81524/modules/items/1416864\n",
+
+    "__Group 13 -- Friday__\n\n" +
+    "10am: https://herts.instructure.com/courses/81524/modules/items/1416899\n" +
+    "11am: https://herts.instructure.com/courses/81524/modules/items/1416900\n" +
+    "1pm: https://teams.microsoft.com/l/meetup-join/19%3ameeting_OWY5OTY0YWYtMTMxOC00NzI3LWI5ZjktM2RhMjE3NzNiOTY5%40thread.v2/0?context=%7b%22Tid%22%3a%2293e6beba-c4aa-4731-af5d-d735b097eadb%22%2c%22Oid%22%3a%220e65d301-8d52-44d5-b211-9d62396d8265%22%7d\n",
+
+];
+
+
+exports.run = function (bot, message, args) {
+    if (args.length !== 1) {
+        return message.reply("Usage: !class [group number]")
+    }
+    try {
+        let group = Number(args[0])-1;
+        if(isNaN(group) || group < 0 || group >= linkData.length){
+            return message.reply("Invalid group.");
+        }
+
+        message.reply("\n\n"+linkData[group])
+
+    } catch (e) {
+        return message.reply("Invalid group.");
+    }
+}
+
+exports.props = {
+    usage: "class [group number]",
+    description: "Gets your class links"
+}
